826e3ed97f72e21111c704bcba9230881309b828
Deryam Hermosilla <deryam.19.07.13@gmail.com>
2021-10-06 09:25:23 -0500

    first commit

---
+++ app/src/main/java/com/example/basicoskotlin/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.basicoskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // variablesYConstantes()
+        tiposDeDatos()
+    }
+    private fun variablesYConstantes() {
+        // Variables
+        var myVariable = "Hola mundo"
+        println(myVariable)
+
+        myVariable = "Bootcamp mobile"
+        println(myVariable)
+
+        // No se puede dar un valor distinto al tipo asignado
+        // myVariable = 10
+
+        // Constantes
+        val myConstant = 10
+        println(myConstant)
+        // No se puede cambiar el valor de la constante
+        // myConstant = 4
+        val mySecondConstant = myVariable
+        println(mySecondConstant)
+    }
+
+    private fun tiposDeDatos() {
+        // String
+        val myText = "Soy una cadena"
+        var text: String
+        text = "Hola"
+        val finalText = text + ", " + myText
+        println(finalText)
+
+        // Enteros (Byte, Short, Int, Long)
+        val myNumber = 10
+        val myNumber2: Int = 5
+        val myFinalNumber = myNumber + myNumber2
+        println(myFinalNumber)
+
+        // Decimales (Float, Double)
+        val myFloat: Float = 12.1f
+
+        // Boolean
+    }
+}
