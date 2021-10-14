1d0bcefb02fc34c9c5e893cedb97936670c1fef4
Deryam Hermosilla <dhermose@everis.com>
2021-10-14 13:31:19 -0500

    condicional when

--- app/src/main/java/com/example/basicoskotlin/MainActivity.kt
+++ app/src/main/java/com/example/basicoskotlin/MainActivity.kt
@@ -9,7 +9,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // variablesYConstantes()
-        tiposDeDatos()
+        // tiposDeDatos()
+        condicionalWhen()
+        // tarea2()
     }
     private fun variablesYConstantes() {
         // Variables
@@ -57,5 +59,93 @@ class MainActivity : AppCompatActivity() {
         val falso: Boolean = false
         println(verdad == falso)
         println(verdad && falso)
+
+        val texto = "abc"
+        for ( chr in texto) {
+            println(chr+1)
+        }
+
     }
+
+    fun condicionalWhen() {
+        val age = 25
+        val word = "k"
+
+        when(age) {
+            0, 1, 2 -> println("Es un bebé")
+            in 3..10 -> println("Es un niño")
+            in 11..14 -> println("Es un puber")
+            in 15..18 -> println("Es un adolescente")
+            in 18..23 -> println("Es un joven")
+            else -> println("Es un adulto")
+        }
+        when(word) {
+            in "a".."h" -> println("primeras")
+            in "i".."m" -> println("intermedias")
+            else -> println("últimas")
+        }
+    }
+
+    // fun List<Int>.promedio(): Double = this.average()
+
+    fun List<Int>.promedio(): Double {
+        val suma = this.sum()
+        val promedio = (suma * 1.0) / this.size
+
+        return promedio
+    }
+    fun tarea2() {
+        //TODO: Crear una función de de extensión que extienda de una Lista que retorne el promedio de los elementos.
+        //Para sacar el promedio se debe seguir las siguientes condiciones:
+        //a) Se debe eliminar la nota mas baja.
+        //b) Se debe duplicar la nota mas alta.
+        //c) Puede haber "errores del sistema" por esa razón tiene que validar que la nota este en el rango de 0 a 20.
+        //d) Si la nota ingresada es mayor a 20 considerar el valor máximo posible, si es menor a 0 considerar el valor mínimo.
+
+        val notes = listOf(13,22,"18",15,"11",-4,16)
+
+        val notas = mutableListOf<Int>()
+
+        for (note in notes) {
+            when(note) {
+                is String -> notas.add(note.toInt())
+                is Int -> notas.add(note)
+            }
+        }
+
+        for ((i, n) in notas.withIndex()) {
+            try {
+                if (n !in 0..20) {
+                    throw Exception("No está en el rango")
+                }
+            } catch (e: Exception) {
+                println(e)
+                if (n < 0) {
+                    notas[i] = 0
+                } else if (n > 20) {
+                    notas[i] = 20
+                }
+            }
+        }
+        notas.sort()
+        notas.remove(notas[0])
+        notas.add(notas[notas.size-1])
+
+        println("La nota es: ${notas.promedio()}")
+    }
+
+
+
+    /*
+    fun estandarizarLista(notes: List<Any>): List<Any> {
+        val notas = mutableListOf<Int>()
+        for (note in notes) {
+            when(note) {
+                is String -> notas.add(note.toInt())
+                is Int -> notas.add(note)
+            }
+        }
+        return  notas
+    }
+    */
 }
---
+++ app/src/main/java/com/example/basicoskotlin/tareas/Tarea1.kt
@@ -0,0 +1,24 @@
+package com.example.basicoskotlin.tareas
+
+fun main() {
+
+    val topics = listOf("¿Qué es Kotlin","Kotlin y Java", "Variables", "Funciones")
+
+    var message = ""
+
+    // TODO: utilizar le metodo listFormatter para dar formato y convertir en texto y asignarlo a la variable message
+
+
+    // TODO: crear un metodo "display" para mostrar un texto en cosola, añadiendo al inicio de del texto "Hoy hemos aprendido: "
+
+    // TODO: utilizar el metodo display y colocar como parametro el nuevo mensaje
+
+}
+
+fun listFormatter (topics: Collection<String>) : String {
+    // TODO: concatenar los elementos de la lista en un cadena de texto separado por comas, usar el metodo String.joinToString()
+    // ref https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/join-to-string.html
+
+    return ""
+}
+
